b96a23e47e5346702d815b14b2ed6ebfce8b133c
Arnav Thakur <129261474+ArnavThakur2004@users.noreply.github.com>
2024-09-29 11:28:25 +0530

    Update Movie_display.py

--- Movie_display.py
+++ Movie_display.py
@@ -9,58 +9,65 @@ import pickle
 import streamlit as st
 import requests
 
+# Function to fetch movie poster using the TMDB API
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+        data = requests.get(url).json()
+        poster_path = data.get('poster_path', '')
+        full_path = f"https://image.tmdb.org/t/p/w500/{poster_path}" if poster_path else ""
+        return full_path
+    except:
+        return ""
 
+# Recommendation function to get movie names and posters
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
+    
+    for i in distances[1:6]:  # Recommend top 5 movies
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
+    
+    return recommended_movie_names, recommended_movie_posters
 
-    return recommended_movie_names,recommended_movie_posters
+# Streamlit app layout
+st.title('Movie Recommender System')
 
-
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+# Load the pre-trained data from pickle files
+movies = pickle.load(open('model/movie_list.pkl', 'rb'))
+similarity = pickle.load(open('model/similarity.pkl', 'rb'))
 
 movie_list = movies['title'].values
+
+# Movie selection dropdown
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
     movie_list
 )
 
+# Display recommendations on button click
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+    with st.spinner('Fetching recommendations...'):
+        recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+        
+        # Displaying movies in a more structured manner
+        if recommended_movie_names:
+            col1, col2, col3, col4, col5 = st.columns(5)
+            cols = [col1, col2, col3, col4, col5]
+            
+            for idx, col in enumerate(cols):
+                col.text(recommended_movie_names[idx])
+                if recommended_movie_posters[idx]:
+                    col.image(recommended_movie_posters[idx], use_column_width=True)
+                else:
+                    col.text("No Image Available")
+        else:
+            st.error('No recommendations found!')
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
-
-
+# Footer section
+st.write("Built with Streamlit by Arnav Thakur")
